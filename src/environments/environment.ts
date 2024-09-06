b97fa7a63083c53597dbde8100248e27b216b7c9
Leonardo Freite <77871584+lmfreite@users.noreply.github.com>
2024-09-06 10:18:12 -0500

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const env={
     production:false,
-  endpoint:"https://localhost:7106/" 
-    //endpoint:"https://segurosabc.netlify.app/" 
+  //endpoint:"https://localhost:7106/" 
+    endpoint:"https://segurosabc.netlify.app/" 
 }
